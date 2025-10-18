22e07c91dea33de9800668b23d16e89625cc7e6f
OleksiiVolynets <pereguznya@gmail.com>
2025-10-18 13:48:43 +0200

    Remove duplicate print statements in Program.cs
    Removed duplicate 'Hello, Alex!' print statements.

--- Git/Program.cs
+++ Git/Program.cs
@@ -1,13 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Alex! One more modification! ");
-Console.WriteLine("Hello, Alex! One more modification! ");
-Console.WriteLine("Hello, Alex! One more modification! ");
-Console.WriteLine("Hello, Alex! One more modification! ");
-Console.WriteLine("Hello, Alex! One more modification! ");
-Console.WriteLine("Hello, Alex! One more modification! ");
-Console.WriteLine("Hello, Alex! One more modification! ");
-Console.WriteLine("Hello, Alex! One more modification! ");
-Console.WriteLine("Hello, Alex! One more modification! ");
-Console.WriteLine("Hello, Alex! One more modification! ");
 
 
